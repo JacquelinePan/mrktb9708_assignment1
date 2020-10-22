62d396fb41cae48180eb4f92508285e25073d0b7
Hunter Dong <hunter.dong@ibm.com>
2020-10-22 19:56:28 -0400

    remove test commit

--- elite_regression.R
+++
@@ -1,4 +0,0 @@
-library(stargazer)
-data = read.csv('assignment1-research-methods.csv', sep = '\t')
-lmElite = glm(calledback ~ ., data = data, family = binomial(link="logit"))
-stargazer(lmElite, title = "Results", align = TRUE)
